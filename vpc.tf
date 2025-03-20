78399c1c4e04a93d444c95e6f0c0c3155f59caf2
Sapana Bassaiye <sapnabasaiye38350@gmail.com>
2025-03-20 16:54:53 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,122 @@
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name    = var.vpc_name
+    Project = var.project_tag
+  }
+}
+
+# Public Subnet 1
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr_1
+  availability_zone       = var.availability_zone_1
+  map_public_ip_on_launch = false  
+
+  tags = {
+    Name    = "${var.vpc_name}-public-subnet_1"
+    Project = var.project_tag
+  }
+}
+
+# Public Subnet 2
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr_2
+  availability_zone       = var.availability_zone_2
+  map_public_ip_on_launch = false  
+
+  tags = {
+    Name    = "${var.vpc_name}-public-subnet_2"
+    Project = var.project_tag
+  }
+}
+
+# Private Subnet
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidr
+  availability_zone       = var.availability_zone_1
+
+  tags = {
+    Name    = "${var.vpc_name}-private-subnet"
+    Project = var.project_tag
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name    = "${var.vpc_name}-igw"
+    Project = var.project_tag
+  }
+}
+
+# Elastic IP for NAT
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name    = "${var.vpc_name}-nat-eip"
+    Project = var.project_tag
+  }
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name    = "${var.vpc_name}-nat"
+    Project = var.project_tag
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name    = "${var.vpc_name}-public-rt"
+    Project = var.project_tag
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name    = "${var.vpc_name}-private-rt"
+    Project = var.project_tag
+  }
+}
+
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Associate Private Subnet with Private Route Table
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
