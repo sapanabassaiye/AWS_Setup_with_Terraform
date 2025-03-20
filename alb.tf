e4bb5a5d03ee7c2061a6ee0df0fff65a0af6de52
Sapana Bassaiye <sapnabasaiye38350@gmail.com>
2025-03-20 16:58:45 +0530

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,44 @@
+resource "aws_lb" "alb" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
+
+# ALB Security Group
+resource "aws_security_group" "alb_sg" {
+  name   = "alb-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+# Allow outbound traffic to private Subnet Instance 1
+resource "aws_security_group_rule" "outbound_traffic_to_privateInstance1" {
+  type                     = "egress"
+  from_port                = 80
+  to_port                  = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.alb_sg.id  # Private instance
+  source_security_group_id = aws_security_group.private_sg_1.id   # Public instance
+  description              = "${var.private_instance_name_1}"
+}
+
